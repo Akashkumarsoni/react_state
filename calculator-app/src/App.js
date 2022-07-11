9e4532a7e4c6452fb5edddb4475c6a6287b8a28d
akashsoni0786 <akashsoni@cedcoss.com>
2022-07-11 08:53:18 +0530

    Calculator

---
+++ calculator-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Calci from './Calci';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Calci />
+      </header>
+    </div>
+  );
+}
+
+export default App;
