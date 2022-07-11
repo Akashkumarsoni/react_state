3f732d0ed6b3bed32b8ddcb5cbf5f287cb7cdb10
akashsoni0786 <akashsoni@cedcoss.com>
2022-07-11 11:36:43 +0530

    Calculator

--- calculator-app/src/Calci.js
+++ calculator-app/src/Calci.js
@@ -6,7 +6,8 @@ export default class Calci extends Component {
     this.state = { data: ''}
 }
 calculate = () => {
-    try {
+    try 
+    {
         const result = eval(this.state.data).toString();
         this.setState({data: result});
     } 
@@ -17,21 +18,26 @@ calculate = () => {
     }
 }
 handleClick = e => {
-    const value = e.target.getAttribute('data-value');
+    const value = e.target.getAttribute('data-value').toString();
     switch(value) {
         case 'clear':
+          console.log(value);
             this.setState({ data: ''});
             break;
         case 'equal':
+          console.log(value);
             this.calculate();
             break;
         case 'delete':
+          console.log(value);
           this.setState({
             data : this.state.data.substring(0,this.state.data.length-1)
           });
           break;
         default:
+          console.log(value);
             this.setState({ data: this.state.data + value});
+            break;
     }
 }
   render() {
@@ -39,31 +45,31 @@ handleClick = e => {
       <div>
         <div id="calculator">
 
-	<div class="top">
-		<span class="clear"><Button onClick={this.handleClick} label="C" value="clear" /></span>
+	<div className="top">
+		<span className="clear"><Button btnclick={this.handleClick} label="C" value="clear" /></span>
   <Display data={this.state.data}/>
 	</div>
 	
-	<div class="keys">
+	<div className="keys">
   
                     
-		<span ><Button onClick={this.handleClick} label="7" value="7" /></span>
-		<span><Button onClick={this.handleClick} label="8" value="8" /></span>
-		<span><Button onClick={this.handleClick} label="9" value="9" /></span>
-		<span class="operator"><Button onClick={this.handleClick} label="+" value="+" /></span>
-		<span><Button onClick={this.handleClick} label="4" value="4" /></span>
-		<span><Button onClick={this.handleClick} label="5" value="5" /></span>
-		<span><Button onClick={this.handleClick} label="6" value="6" /></span>
-		<span class="operator"><Button onClick={this.handleClick} label="-" value="-" /></span>
-		<span><Button onClick={this.handleClick} label="1" value="1" /></span>
-		<span><Button onClick={this.handleClick} label="2" value="2" /></span>
-		<span><Button onClick={this.handleClick} label="3" value="3" /></span>
-		<span class="operator"><Button onClick={this.handleClick} label="÷" value="/" /></span>
-		<span><Button onClick={this.handleClick} label="0" value="0" /></span>
-		<span><Button onClick={this.handleClick} label="." value="." /></span>
-		<span class="eval"><Button onClick={this.handleClick} label="=" value="equal" /></span>
-		<span class="operator"><Button onClick={this.handleClick} label="x" value="*" /></span>
-    <span class="del"><Button onClick={this.handleClick} label="Del" value="delete" /></span>
+		<span ><Button btnclick={this.handleClick} label="7" value="7" /></span>
+		<span><Button btnclick={this.handleClick} label="8" value="8" /></span>
+		<span><Button btnclick={this.handleClick} label="9" value="9" /></span>
+		<span className="operator"><Button btnclick={this.handleClick} label="+" value="+" /></span>
+		<span><Button btnclick={this.handleClick} label="4" value="4" /></span>
+		<span><Button btnclick={this.handleClick} label="5" value="5" /></span>
+		<span><Button btnclick={this.handleClick} label="6" value="6" /></span>
+		<span className="operator"><Button btnclick={this.handleClick} label="-" value="-" /></span>
+		<span><Button btnclick={this.handleClick} label="1" value="1" /></span>
+		<span><Button btnclick={this.handleClick} label="2" value="2" /></span>
+		<span><Button btnclick={this.handleClick} label="3" value="3" /></span>
+		<span className="operator"><Button btnclick={this.handleClick} label="÷" value="/" /></span>
+		<span><Button btnclick={this.handleClick} label="0" value="0" /></span>
+		<span><Button btnclick={this.handleClick} label="." value="." /></span>
+		<span className="eval"><Button btnclick={this.handleClick} label="=" value="equal" /></span>
+		<span className="operator"><Button btnclick={this.handleClick} label="x" value="*" /></span>
+    <span className="del"><Button btnclick={this.handleClick} label="Del" value="delete" /></span>
 	</div>
 </div>
       </div>
@@ -73,7 +79,7 @@ handleClick = e => {
 class Display extends Component {
   render(){
       return(
-          <div class="screen">
+          <div className="screen">
               {this.props.data}
           </div>
       );
@@ -85,7 +91,7 @@ class Button extends Component {
       return(
           <div 
               className="Buttons"
-              onClick={this.props.onClick}
+              onClick={this.props.btnclick}
               data-size={this.props.size}
               data-value={this.props.value}>
               {this.props.label}
